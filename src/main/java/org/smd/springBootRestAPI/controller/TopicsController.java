4b11c248de0f0789d649f053cd09438ab4988162
unknown <shimada.henrique@gmail.com>
2020-08-24 22:28:18 -0300

    POST method implemented for Topic registration

--- src/main/java/org/smd/springBootRestAPI/controller/TopicsController.java
+++ src/main/java/org/smd/springBootRestAPI/controller/TopicsController.java
@@ -2,6 +2,7 @@ package org.smd.springBootRestAPI.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.smd.springBootRestAPI.controller.dto.TopicDTO;
 import org.smd.springBootRestAPI.controller.dto.TopicForm;
@@ -12,9 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -42,19 +45,42 @@ public class TopicsController {
 
 	}
 
+	@GetMapping("/{id}") // Setting an {id}: String as endpoint creates a new endpoint
+	public Optional<Topic> listChosen(@PathVariable Long id) { // As specific Topic may be requested via URI endpoint.
+														// @PathVariable assumes that the param id is named the same as
+														// the endpoint. Whenever the method param differs from the
+														// endpoint, @PathVariable must be set: @PathVariable("param")
+		 
+		return topicsRepository.findById(id); // Uses as default Spring Method
+
+	}
+
 	@PostMapping
 	@Transactional
-	public ResponseEntity<TopicDTO> register(@RequestBody TopicForm topicForm, UriComponentsBuilder uriBuilder) { // ResponseEntity<TopicDTO> is a Http dealer type with a specified generic validation
+	public ResponseEntity<TopicDTO> register(@RequestBody TopicForm topicForm, UriComponentsBuilder uriBuilder) { // ResponseEntity<TopicDTO>
+																													// is
+																													// a
+																													// Http
+																													// dealer
+																													// type
+																													// with
+																													// a
+																													// specified
+																													// generic
+																													// validation
 		//
-		
-		Topic topic = topicForm.toTopic(courseRepository); // Passes a CourseRepository as void param from topicForm(DTO)
+
+		Topic topic = topicForm.toTopic(courseRepository); // Passes a CourseRepository as void param from
+															// topicForm(DTO)
 		topicsRepository.save(topic);
-		
+
 		URI uri = uriBuilder.path("/topics/{id}") // sends a parametrized HTTP GET request
-				.buildAndExpand(topic.getId()) // exchanges the {id} part from the earlier path() with the returned getId() 
-				.toUri(); // creates the requested URI tobe called 
-		
-		return ResponseEntity.created(uri) // created() returns a HTTP 201 with the location e requires an URI with call for object representation of the created object
+				.buildAndExpand(topic.getId()) // exchanges the {id} part from the earlier path() with the returned
+												// getId()
+				.toUri(); // creates the requested URI tobe called
+
+		return ResponseEntity.created(uri) // created() returns a HTTP 201 with the location e requires an URI with call
+											// for object representation of the created object
 				.body(new TopicDTO(topic)); // builds the body of the response
 	}
 
