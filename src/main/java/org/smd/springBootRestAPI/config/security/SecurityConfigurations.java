7fca7f5aa5dd87a7f4fb41dcc29f507a0bcbd9d2
hshima <shimada.henrique@gmail.com>
2020-11-01 22:02:23 -0300

    Includes Security configuration

---
+++ src/main/java/org/smd/springBootRestAPI/config/security/SecurityConfigurations.java
@@ -0,0 +1,8 @@
+package org.smd.springBootRestAPI.config.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+}
