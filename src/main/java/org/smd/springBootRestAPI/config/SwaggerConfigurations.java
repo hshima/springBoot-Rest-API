a301820cc37df344e1e4c90e4e2c06c381c2e501
hshima <shimada.henrique@gmail.com>
2020-11-02 22:21:56 -0300

    Adds Token field for succesful submit

--- src/main/java/org/smd/springBootRestAPI/config/SwaggerConfigurations.java
+++ src/main/java/org/smd/springBootRestAPI/config/SwaggerConfigurations.java
@@ -1,11 +1,15 @@
 package org.smd.springBootRestAPI.config;
 
+import java.util.Arrays;
+
 import org.smd.springBootRestAPI.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -20,6 +24,17 @@ public class SwaggerConfigurations {
 				.paths(PathSelectors.ant("/**"))
 				.build()
 				.ignoredParameterTypes(User.class)
+				.globalOperationParameters(Arrays.asList(
+						new ParameterBuilder()
+						.name("Authorization")
+						.description("Header for JWT Token")
+						.modelRef(new ModelRef("string"))
+						.parameterType("header")
+						.required(false)
+						.build(
+						)
+					)
+				)
 				;
 	}
 }
